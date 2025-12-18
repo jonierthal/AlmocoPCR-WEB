8e37e142ca72b51c37b98bed3e2cba840b8c5a59
Jonathan Erthal <jonierthal@gmail.com>
2025-12-18 08:37:17 -0300

    Configure local backend execution

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,5 +1,8 @@
 import axios from "axios";
 
+const baseURL =
+    import.meta.env.VITE_API_BASE_URL ?? "https://appalmoco-pcr.azurewebsites.net/";
+
 export const api = axios.create({
-    baseURL: 'https://appalmoco-pcr.azurewebsites.net/'
-})
+    baseURL,
+});
